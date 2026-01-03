2eab57790dcbb6e58a089015d92dfbbb12982051
sheenaanto <sheena.anto08@gmail.com>
2026-01-03 20:08:13 +0000

    Update ALLOWED_HOSTS to include localhost and Heroku subdomains

--- taskmaster/settings.py
+++ taskmaster/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['taskmaster.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com',
+                 '127.0.0.1',]
 
 
 # Application definition
